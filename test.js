d2862714cb67832cf2c7ea0e52f8dcf7afd19035
Sejj42 <omer162@gmail.com>
2022-03-07 15:44:47 +0200

    table v1.0.0 works

--- test.js
+++ test.js
@@ -24,15 +24,6 @@ const getAllFeatures = async (pageData) => {
   let allFeatures = [];
 
   await pageData.results.forEach((result) => {
-    // console.log(
-    //   util.inspect(
-    //     result.properties.gap.formula.number,
-    //     false,
-    //     null,
-    //     true /* enable colors */
-    //   )
-    // );
-    // console.log("-----------------------------------------");
     try {
       if (
         result.properties.Type &&
@@ -75,13 +66,6 @@ const getAllFeatures = async (pageData) => {
             result.properties.Related.relation.length !== 0
               ? result.properties.Related.relation
               : "N/A",
-
-          // owner: result.properties.Owner.multi_select[0].name,
-          // // featureName: result.properties[" task"].title[0],
-          // score: scoreValidator(result.properties.gap),
-          // sprint: result.properties.Sprint
-          //   ? result.properties.Sprint.select.name
-          //   : "N/A",
         });
       } else {
         return `${result.id} isn't a feature OR name is null`;
@@ -93,12 +77,100 @@ const getAllFeatures = async (pageData) => {
   return allFeatures;
 };
 
+const allFeaturesEntriesGenerator = async (allFeatures) => {
+  const allFeaturesEntries = allFeatures.map((feature) => {
+    return {
+      Name: {
+        title: [
+          {
+            text: {
+              content: feature.owner,
+            },
+          },
+        ],
+      },
+      featureID: {
+        type: "rich_text",
+        rich_text: [
+          {
+            text: {
+              content: feature.featureId,
+            },
+          },
+        ],
+      },
+      featureName: {
+        type: "rich_text",
+        rich_text: [
+          {
+            text: {
+              content: feature.featureName,
+            },
+          },
+        ],
+      },
+      Score: {
+        type: "rich_text",
+        rich_text: [
+          {
+            text: {
+              content: feature.score,
+            },
+          },
+        ],
+      },
+      Sprint: {
+        type: "rich_text",
+        rich_text: [
+          {
+            text: {
+              content: feature.sprint,
+            },
+          },
+        ],
+      },
+      // Related: {
+      //   type: "rich_text",
+      //   rich_text: [
+      //     {
+      //       text: {
+      //         content: feature.related,
+      //       },
+      //     },
+      //   ],
+      // },
+    };
+  });
+  return allFeaturesEntries;
+};
+
+const entryAdder = async (allFeaturesEntries) => {
+  await allFeaturesEntries.forEach((entry) => addSingleEntry(entry));
+};
+
+const addSingleEntry = async (entry) => {
+  try {
+    const response = await notion.pages.create({
+      parent: {
+        database_id: process.env.NOTION_PERFORMANCE_LIST_DATABASE_ID,
+      },
+      properties: entry,
+    });
+    console.log(response);
+    console.log("Entry added successfully.");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const indexFunction = async () => {
   const pageData = await getPagedata();
   const allFeatures = await getAllFeatures(pageData);
-  console.log(util.inspect(allFeatures, false, null, true /* enable colors */));
-  // const allFeatureEntries = await allFeaturesEntriesGenerator(allFeatures);
-  // await entryAdder(allFeatureEntries);
+  const allFeatureEntries = await allFeaturesEntriesGenerator(allFeatures);
+  console.log(
+    util.inspect(allFeatureEntries, false, null, true /* enable colors */)
+  );
+  await entryAdder(allFeatureEntries);
 
   return "index function ran successfully.";
 };
