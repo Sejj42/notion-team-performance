28718bc1437917db17e58bb8552d8465e39c6b72
Sejj42 <omer162@gmail.com>
2022-03-07 18:05:21 +0200

    removed redundant code

--- index.js
+++ index.js
@@ -11,32 +11,84 @@ const getPagedata = async () => {
 };
 
 const getAllFeatures = async (pageData) => {
-  const scoreValidator = (gap) => {
-    if (gap && gap.number !== 0) {
-      return (gap.number * -1).toString();
-    } else if (gap && gap.number == 0) {
+  const thePage = pageData[Object.keys(pageData)[1]];
+
+  const getGapSumOfFeatureTasks = (featureTasksArray) => {
+    let featureGap = 0;
+    featureTasksArray.forEach((task) => {
+      thePage.forEach((pageTask) => {
+        if (task.id === pageTask.id) {
+          if (
+            pageTask.properties.gap &&
+            pageTask.properties.gap.formula &&
+            pageTask.properties.gap.formula.number != undefined &&
+            pageTask.properties.gap.formula.number != null
+          ) {
+            featureGap += pageTask.properties.gap.formula.number;
+          } else {
+            featureGap += 0;
+          }
+          return pageTask.id;
+        }
+      });
+    });
+    if (featureGap === 0) {
       return "0";
     } else {
-      return "N/A";
+      return String(featureGap * -1);
     }
   };
 
   let allFeatures = [];
 
   await pageData.results.forEach((result) => {
-    if (
-      result.properties.Type &&
-      result.properties.Type.select.name === "Feature"
-    ) {
-      allFeatures.push({
-        owner: result.properties.Owner.multi_select[0].name,
-        featureId: result.id,
-        // featureName: result.properties[" task"].title[0],
-        score: scoreValidator(result.properties.gap),
-        sprint: result.properties.Sprint
-          ? result.properties.Sprint.select.name
-          : "N/A",
-      });
+    try {
+      if (
+        result.properties.Type &&
+        result.properties.Type.select != null &&
+        result.properties.Type.select.name != undefined &&
+        result.properties.Type.select.name != null &&
+        result.properties.Type.select.name === "Feature"
+      ) {
+        allFeatures.push({
+          owner:
+            result.properties.Owner.people[0] &&
+            result.properties.Owner.people[0].name
+              ? result.properties.Owner.people[0].name
+              : `userID: ${result.properties.Owner.people[0].id}`,
+
+          featureId: result.id,
+
+          featureName: result.properties.task.title[0].text.content,
+
+          sprint:
+            result.properties.Sprint &&
+            result.properties.Sprint.multi_select &&
+            result.properties.Sprint.multi_select.length !== 0 &&
+            result.properties.Sprint.multi_select[0].name !== undefined &&
+            result.properties.Sprint.multi_select[0].name !== null
+              ? result.properties.Sprint.multi_select[0].name
+              : "N/A",
+
+          related:
+            result.properties.Related &&
+            result.properties.Related.relation &&
+            result.properties.Related.relation.length !== 0
+              ? result.properties.Related.relation
+              : "N/A",
+
+          score:
+            result.properties.Related &&
+            result.properties.Related.relation &&
+            result.properties.Related.relation.length !== 0
+              ? getGapSumOfFeatureTasks(result.properties.Related.relation)
+              : "N/A",
+        });
+      } else {
+        return `${result.id} isn't a feature OR name is null`;
+      }
+    } catch (error) {
+      console.error(error);
     }
   });
   return allFeatures;
@@ -54,12 +106,12 @@ const allFeaturesEntriesGenerator = async (allFeatures) => {
           },
         ],
       },
-      featureID: {
+      featureName: {
         type: "rich_text",
         rich_text: [
           {
             text: {
-              content: feature.featureId,
+              content: feature.featureName,
             },
           },
         ],
@@ -84,6 +136,16 @@ const allFeaturesEntriesGenerator = async (allFeatures) => {
           },
         ],
       },
+      // Related: {
+      //   type: "rich_text",
+      //   rich_text: [
+      //     {
+      //       text: {
+      //         content: feature.related,
+      //       },
+      //     },
+      //   ],
+      // },
     };
   });
   return allFeaturesEntries;
